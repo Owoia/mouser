09c559bf686251dc0658129be405bc1666c2d1c1
Owoia <emailidiotguybutreal@gmail.com>
2025-04-02 19:49:43 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,38 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+f f f f f f f f f 1 1 2 
+f c b b b b b b b 2 1 2 
+f b c b b b b b b 1 1 1 
+f d b c b b b b c b d f 
+f d d b c b b c b d d f 
+f d d d b c c b d d d f 
+f d d d d b b d d d d f 
+f d d d d d d d d d d f 
+f d d d d d d d d d d f 
+f f f f f f f f f f f f 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
